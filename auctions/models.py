e4cdaa3d17b2aebaa67535cfd47db8e924260a73
Thabo Mbatha <thabothomasmbatha@gmail.com>
2024-07-17 15:44:56 +0200

    Project 2

---
+++ auctions/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+# class auctionListings(request):
+#     pass
+
+# class bids(request):
+#     pass
+
+# class comments(request):
+#     pass
