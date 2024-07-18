2cf4053d9ee45c67eef7f7ff13deeaf4d870472b
Thabo Mbatha <thabothomasmbatha@gmail.com>
2024-07-18 07:44:38 +0200

    user model

--- auctions/models.py
+++ auctions/models.py
@@ -3,7 +3,9 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    
+    def __str__(self):
+        return "@{}".format(self.username)
 
 # class auctionListings(request):
 #     pass
